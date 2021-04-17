bf12abd13a73a3ebe4c0d16a359b2ad1d2f21707
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-04-17 23:42:07 +0200

    wqd

--- 7.4/entrypoint.sh
+++ 7.4/entrypoint.sh
@@ -72,8 +72,8 @@ echo "FLUSH PRIVILEGES;" | mysql --user=root
 chmod -R +r /var/log/
 cd /var/www/html/tests
 
-echo " ----- /var/www/html/vendor/bin/phpunit --verbose --colors=always --log-junit '/var/www/html/tests/phpunit.xml' --testsuite Test1   -----"
-/var/www/html/vendor/bin/phpunit --verbose --colors=always --log-junit '/var/www/html/tests/phpunit.xml' --testsuite Test1 
+echo " ----- /var/www/html/vendor/bin/phpunit --verbose --colors=always --testsuite Init,Settings,Base,Integrations,Apps    -----"
+/var/www/html/vendor/bin/phpunit --verbose --colors=always --testsuite Init,Settings,Base,Integrations,Apps 
 
 
 if [ "$COVERAGE" == "true" ]; then
