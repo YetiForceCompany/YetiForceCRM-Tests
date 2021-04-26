6ff72f7588caa4c7ef9f4068a15873ac28af98ba
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-04-26 13:16:14 +0200

    7.4/entrypoint.sh

--- 7.4/entrypoint.sh
+++ 7.4/entrypoint.sh
@@ -94,7 +94,7 @@ if [ "$COVERAGE" == "true" ]; then
 	
 	cp -R /var/www/html/* $GITHUB_WORKSPACE
 	echo " ----- bash <(curl -s https://codecov.io/bash) -f /var/www/html/tests/coverages/coverage2.xml  -----"
-	bash <(curl -s https://codecov.io/bash) -f /var/www/html/tests/coverages/coverage2.xml -R /
+	bash <(curl -s https://codecov.io/bash) -f /var/www/html/tests/coverages/coverage2.xml -k /var/www/html/
 	
 	echo " ----- bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r /var/www/html/tests/coverages/coverage3.xml  -----"
 	bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r /var/www/html/tests/coverages/coverage3.xml
