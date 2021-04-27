778b48f7e4f2a7b1319006c7c0cef87abc7b3399
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-04-27 08:23:10 +0200

    7.4/entrypoint.sh

--- 7.4/entrypoint.sh
+++ 7.4/entrypoint.sh
@@ -90,8 +90,8 @@ if [ "$COVERAGE" == "true" ]; then
 	
 	ls -all /var/www/html/
 
-	echo " ----- bash <(curl -s https://codecov.io/bash) -f /var/www/html/tests/coverages/coverage.xml -R /var/www/html/  -----"
-	bash <(curl -s https://codecov.io/bash) -f /var/www/html/tests/coverages/coverage.xml -R /var/www/html/
+	echo " ----- bash <(curl -s https://codecov.io/bash) -f /var/www/html/tests/coverages/coverage2.xml -R /var/www/html/  -----"
+	bash <(curl -s https://codecov.io/bash) -f /var/www/html/tests/coverages/coverage2.xml -R /var/www/html/
 	
 	echo " ----- bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r /var/www/html/tests/coverages/coverage3.xml  -----"
 	bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r /var/www/html/tests/coverages/coverage3.xml
