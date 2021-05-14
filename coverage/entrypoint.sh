cc919d565ea6c56ddd17abb2f7aa6010cf4c3d19
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-05-15 00:19:07 +0200

    Update entrypoint.sh

--- coverage/entrypoint.sh
+++ coverage/entrypoint.sh
@@ -79,7 +79,7 @@ chmod -R +r /var/log/
 cd /var/www/html/tests
 
 echo " ----- /var/www/html/vendor/bin/phpunit --verbose --colors=always --testsuite All    -----"
-/var/www/html/vendor/bin/phpunit --verbose --colors=always --log-junit '/var/www/html/tests/coverages/execution.xml' --testsuite All --no-progress --only-covered
+/var/www/html/vendor/bin/phpunit --verbose --colors=always --log-junit '/var/www/html/tests/coverages/execution.xml' --testsuite All --only-covered
 
 
 if [ "$COVERAGE" == "true" ]; then
